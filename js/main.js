ad9bdad1697c0d1bca307870c1095d0010fda4a6
DoccHik <evgeniy161102@gmail.com>
2022-04-29 20:40:09 +0300

    add modal window

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const menuIcon = document.querySelector('.header__menu-icon');
 const menuBody = document.querySelector('.header__menu-body');
 // Тег body 
 const bodyLock = document.querySelector('body');
-// console.log(bodyLock)
+
 
 // Событие при клике на меню-бургер
 menuIcon.addEventListener('click', function() {
@@ -30,6 +30,36 @@ menuIcon.addEventListener('click', function() {
         });
     };
 });
+
+// Модальне окна
+
+const modalButtons = document.querySelectorAll('[data-modal-button]');
+const modalButtonsClose = document.querySelectorAll('[data-modal-close]');
+const allModals = document.querySelectorAll('[data-modal]');
+
+
+modalButtons.forEach((item) => {
+    item.addEventListener('click', function() {
+        const modalId = this.dataset.modalButton;
+        console.log(item);
+        console.log(modalId);
+
+        const modal = document.querySelector('#' + modalId);
+        console.log(modal);
+
+        modal.classList.remove('hidden-modal');
+        bodyLock.classList.add('lock');
+    })
+});
+
+modalButtonsClose.forEach((item) => {
+    item.addEventListener('click', function() {
+        const modal = this.closest('[data-modal]');
+        modal.classList.add('hidden-modal');
+        bodyLock.classList.remove('lock');
+    })
+})
+
 
 // Прокрутка при клике Jquery
 
